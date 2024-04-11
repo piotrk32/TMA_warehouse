f115c0428f0582d829251dc0d400871703974d9f
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 12:02:34 +0200

    dtos for Item added

---
+++ src/main/java/com/example/tma_warehouse/models/item/enums/ItemGroup.java
@@ -0,0 +1,5 @@
+package com.example.tma_warehouse.models.item.enums;
+
+public enum ItemGroup {
+    GROUP_A, GROUP_B, GROUP_C
+}
---
+++ src/main/java/com/example/tma_warehouse/models/item/enums/UnitOfMeasurement.java
@@ -0,0 +1,5 @@
+package com.example.tma_warehouse.models.item.enums;
+
+public enum UnitOfMeasurement {
+    t, kg, m, g, s, mol
+}
