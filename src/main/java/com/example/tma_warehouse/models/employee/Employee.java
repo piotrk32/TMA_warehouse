f5535962908d5984c84092edb7256349a4511f78
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 10:51:50 +0200

    Employee udpated, yml updated

--- src/main/java/com/example/tma_warehouse/models/employee/Employee.java
+++ src/main/java/com/example/tma_warehouse/models/employee/Employee.java
@@ -1,6 +1,34 @@
 package com.example.tma_warehouse.models.employee;
 
+import com.example.tma_warehouse.models.role.Role;
 import com.example.tma_warehouse.models.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "providers")
+@Getter
+@Setter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Employee extends User {
+
+    public Employee(String firstName,
+                    String lastName,
+                    LocalDate birthDate,
+                    String email,
+                    String phoneNumber,
+                    String accessToken,
+                    String refreshToken,
+                    String idToken,
+                    Role role) {
+        super(firstName, lastName, birthDate, email, phoneNumber, accessToken, refreshToken, idToken, role);
+    }
 }
