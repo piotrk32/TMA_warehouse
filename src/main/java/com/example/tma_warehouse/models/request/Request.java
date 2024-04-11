949c7aec94edca161b7e10d2c15ff8f07958e699
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 13:32:14 +0200

    Request updated

--- src/main/java/com/example/tma_warehouse/models/request/Request.java
+++ src/main/java/com/example/tma_warehouse/models/request/Request.java
@@ -3,12 +3,15 @@ package com.example.tma_warehouse.models.request;
 import com.example.tma_warehouse.models.basic.BasicEntity;
 import com.example.tma_warehouse.models.employee.Employee;
 import com.example.tma_warehouse.models.item.Item;
+import com.example.tma_warehouse.models.item.enums.UnitOfMeasurement;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "requests")
@@ -29,6 +32,24 @@ public class Request extends BasicEntity {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id", referencedColumnName = "id")
     Item item;
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "request_row_id_generator")
+    @SequenceGenerator(name = "request_row_id_generator", sequenceName = "request_row_id_seq", allocationSize = 1)
+    @Column(name = "request_row_id", updatable = false, nullable = false)
+    private Long requestRowId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "unit_of_measurement")
+    private UnitOfMeasurement unitOfMeasurement; // Założenie, że UnitOfMeasurement to Enum
+
+    @Column(name = "quantity")
+    private BigDecimal quantity;
+
+    @Column(name = "price_without_vat")
+    private BigDecimal priceWithoutVAT;
+
+    @Column(name = "comment")
+    private String comment;
 
 
 
