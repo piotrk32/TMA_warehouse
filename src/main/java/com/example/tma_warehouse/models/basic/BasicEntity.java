c482e6465a15da8f506f7991b8ae4772d8e50da8
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 08:45:32 +0200

    basic entity created

---
+++ src/main/java/com/example/tma_warehouse/models/basic/BasicEntity.java
@@ -0,0 +1,35 @@
+package com.example.tma_warehouse.models.basic;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class BasicEntity {
+
+    private static final String SEQUENCE_NAME = "id_seq";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "base_generator")
+    @SequenceGenerator(name = "base_generator", sequenceName = SEQUENCE_NAME, allocationSize = 1)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+
+
+}
