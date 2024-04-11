aff62904002abe885540efd0bc3eca71eb2eca1f
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 10:08:22 +0200

    UserService created, basic methods added

---
+++ src/main/java/com/example/tma_warehouse/services/UserService.java
@@ -0,0 +1,38 @@
+package com.example.tma_warehouse.services;
+
+import com.example.tma_warehouse.exceptions.EntityNotFoundException;
+import com.example.tma_warehouse.models.role.Role;
+import com.example.tma_warehouse.models.user.User;
+import com.example.tma_warehouse.repositories.RoleRepository;
+import com.example.tma_warehouse.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public User getUserByEmail(String email) {
+        Optional<User> userOptional = userRepository.findUserByEmail(email);
+        if (!userOptional.isPresent()) {
+            throw new EntityNotFoundException("User", "No user found with email: " + email);
+        }
+        return userOptional.get();
+    }
+
+
+    public void addRoleToUser(String email, String roleName) {
+        User user = userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+        Role role = roleRepository.findByName(roleName)
+                .orElseThrow(() -> new IllegalArgumentException("Role Not Found: " + roleName));
+
+        user.setRole(role); // Ustawienie pojedynczej roli zamiast dodawania do zbioru
+        userRepository.save(user);
+    }
+}
