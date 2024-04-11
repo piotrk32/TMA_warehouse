307b7c88cba83f9fe2415f54de47865669cba3a8
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 17:17:24 +0200

    UserInputDTO with basic validation created

---
+++ src/main/java/com/example/tma_warehouse/models/user/dtos/UserInputDTO.java
@@ -0,0 +1,30 @@
+package com.example.tma_warehouse.models.user.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+
+@Getter
+public class UserInputDTO {
+
+    @NotBlank(message = "First name field must be filled in.")
+    @Length(min = 2, max = 255, message = "First name must be between 2 and 255 characters.")
+    private String firstName;
+
+    @NotBlank(message = "Last name field must be filled in.")
+    @Length(min = 2, max = 255, message = "Last name must be between 2 and 255 characters.")
+    private String lastName;
+
+    @NotNull(message = "Birth date field must be filled in.")
+    private LocalDate birthDate;
+
+    @NotBlank(message = "Phone number field must be filled in.")
+    @Pattern(regexp = "\\d+", message = "Phone number must contain only digits.")
+    @Length(min = 7, max = 15, message = "Phone number must be between 7 and 15 digits.")
+    private String phoneNumber;
+
+}
