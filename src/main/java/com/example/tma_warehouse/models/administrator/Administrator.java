1e44f56a9e1f387368b7c11fd61ef19335531cf3
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 13:31:37 +0200

    Administrator changed

--- src/main/java/com/example/tma_warehouse/models/administrator/Administrator.java
+++ src/main/java/com/example/tma_warehouse/models/administrator/Administrator.java
@@ -1,6 +1,21 @@
 package com.example.tma_warehouse.models.administrator;
 
 import com.example.tma_warehouse.models.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class Administrator extends User {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>(super.getAuthorities());
+        // Dodanie dodatkowych uprawnień dla administratora
+        authorities.add(new SimpleGrantedAuthority("ROLE_ADMINISTRATOR"));
+        // Możesz dodać więcej uprawnień specyficznych dla roli Administratora
+        return authorities;
+    }
 }
