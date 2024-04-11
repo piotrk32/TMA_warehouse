0d0336a768753e563e717ec2f42a0d9134f50f63
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 08:37:02 +0200

    project built, dependencies added

---
+++ src/main/java/com/example/tma_warehouse/TmaWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.tma_warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmaWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmaWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tma_warehouse/TmaWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tma_warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmaWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
