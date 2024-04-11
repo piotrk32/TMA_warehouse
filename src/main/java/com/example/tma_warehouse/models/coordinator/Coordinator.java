ece87147c6862996d85b8347d86be3a50c2572a3
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-11 13:31:55 +0200

    constructor added

--- src/main/java/com/example/tma_warehouse/models/coordinator/Coordinator.java
+++ src/main/java/com/example/tma_warehouse/models/coordinator/Coordinator.java
@@ -1,6 +1,21 @@
 package com.example.tma_warehouse.models.coordinator;
 
+import com.example.tma_warehouse.models.role.Role;
 import com.example.tma_warehouse.models.user.User;
 
+import java.time.LocalDate;
+
 public class Coordinator extends User {
+
+    public Coordinator(String firstName,
+                    String lastName,
+                    LocalDate birthDate,
+                    String email,
+                    String phoneNumber,
+                    String accessToken,
+                    String refreshToken,
+                    String idToken,
+                    Role role) {
+        super(firstName, lastName, birthDate, email, phoneNumber, accessToken, refreshToken, idToken, role);
+    }
 }
