c0e9f149f0ac79223a651b526fdca7c871a6f817
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-13 23:37:22 +0200

    repo updated

--- src/main/java/com/example/tma_warehouse/repositories/RequestRepository.java
+++ src/main/java/com/example/tma_warehouse/repositories/RequestRepository.java
@@ -2,6 +2,7 @@ package com.example.tma_warehouse.repositories;
 
 import com.example.tma_warehouse.models.request.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RequestRepository extends JpaRepository<Request, Long> {
+public interface RequestRepository extends JpaRepository<Request, Long>, JpaSpecificationExecutor<Request> {
 }
