970a85e0a22c5c4fcf848d11cda73ba13c911e69
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-13 20:25:55 +0200

    ItemRepository changed

--- src/main/java/com/example/tma_warehouse/repositories/ItemRepository.java
+++ src/main/java/com/example/tma_warehouse/repositories/ItemRepository.java
@@ -1,8 +1,9 @@
 package com.example.tma_warehouse.repositories;
 
 import com.example.tma_warehouse.models.item.Item;
-import com.example.tma_warehouse.models.role.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
 }
