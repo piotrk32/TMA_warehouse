b42396b9d1ed236eb3b7fa383e5f05de653eb87c
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-14 16:04:18 +0200

    RequestInputDTO updated

--- src/main/java/com/example/tma_warehouse/models/request/dtos/RequestInputDTO.java
+++ src/main/java/com/example/tma_warehouse/models/request/dtos/RequestInputDTO.java
@@ -1,7 +1,6 @@
 package com.example.tma_warehouse.models.request.dtos;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
@@ -11,20 +10,10 @@ public record RequestInputDTO(
         @NotNull(message = "Item ID cannot be null.")
         Long itemId,
 
-        @NotBlank(message = "Unit of measurement must be filled in.")
-        String unitOfMeasurement,
-
         @NotNull(message = "Quantity cannot be empty.")
         @Min(value = 0, message = "Quantity must not be less than zero.")
         BigDecimal quantity,
-
-        @NotNull(message = "Price without VAT cannot be null.")
-        @Min(value = 0, message = "Price must not be less than zero.")
-        BigDecimal priceWithoutVat,
-
         String comment
-        // Status and Request Row ID are likely to be managed by the backend logic and not provided by the user.
+
 ) {
-    // The record implicitly includes all the fields in its constructor,
-    // and the corresponding getters are also generated automatically.
 }
