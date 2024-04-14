ebbec878ba417be19d3801c4b7c6e0b640c6edc5
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-14 18:20:28 +0200

    Api response added

---
+++ src/main/java/com/example/tma_warehouse/utils/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.example.tma_warehouse.utils;
+
+public class ApiResponse<T> {
+    private T data;
+    private String message;
+
+    public ApiResponse(T data, String message) {
+        this.data = data;
+        this.message = message;
+    }
+
+    // Getters and setters
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
