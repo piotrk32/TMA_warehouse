a2ec9982caa26ca8fd7f5a438c52ebb6a1361ac7
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-15 12:09:30 +0200

    RowRequestFacade updated

--- src/main/java/com/example/tma_warehouse/services/rowrequest/RowRequestFacade.java
+++ src/main/java/com/example/tma_warehouse/services/rowrequest/RowRequestFacade.java
@@ -3,15 +3,23 @@ package com.example.tma_warehouse.services.rowrequest;
 import com.example.tma_warehouse.models.RowRequest.RowRequest;
 import com.example.tma_warehouse.models.RowRequest.dtos.RowRequestInputDTO;
 import com.example.tma_warehouse.models.RowRequest.dtos.RowRequestResponseDTO;
+import com.example.tma_warehouse.models.request.Request;
+import com.example.tma_warehouse.models.request.dtos.RequestResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import static com.example.tma_warehouse.models.RowRequest.dtos.RowRequestMapper.mapToRowRequestResponseDTO;
+import static com.example.tma_warehouse.models.request.dtos.RequestMapper.mapToRequestResponseDTO;
 
 @Component
 @RequiredArgsConstructor
 public class RowRequestFacade {
     private final RowRequestService rowRequestService;
+
+    public RowRequestResponseDTO getRowRequestById(Long rowRequestId) {
+        RowRequest rowRequest = rowRequestService.getRowRequestById(rowRequestId);
+        return mapToRowRequestResponseDTO(rowRequest);
+    }
 
 
     public RowRequestResponseDTO addItemToRequest(Long requestId, RowRequestInputDTO rowRequestInputDTO) {
