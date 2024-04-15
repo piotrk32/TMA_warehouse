93f8153ba9ee59e3fcfb1ce894e4d9dc01476750
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-15 13:02:31 +0200

    Date of creating request added

--- src/main/java/com/example/tma_warehouse/models/request/dtos/RequestMapper.java
+++ src/main/java/com/example/tma_warehouse/models/request/dtos/RequestMapper.java
@@ -2,10 +2,14 @@ package com.example.tma_warehouse.models.request.dtos;
 
 import com.example.tma_warehouse.models.request.Request;
 
+import java.time.LocalDateTime;
+
+
 public class RequestMapper {
 
     public static RequestResponseDTO mapToRequestResponseDTO(Request request) {
         String fullName = request.getEmployee().getFirstName() + " " + request.getEmployee().getLastName();
+
 
         return RequestResponseDTO
                 .builder()
@@ -14,6 +18,10 @@ public class RequestMapper {
                 .priceWithoutVat(request.getPriceWithoutVAT())
                 .comment(request.getComment())
                 .status(request.getStatus().name())
+                .createdAt(LocalDateTime.parse(request.getCreatedAt().toString()))
+                .modifiedAt(LocalDateTime.parse(request.getModifiedAt().toString()))
                 .build();
     }
+
+
 }
