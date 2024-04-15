fc10219f41428ecd170b9606556f9a5f543ebc0a
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-16 00:23:36 +0200

    Repository updated

--- src/main/java/com/example/tma_warehouse/repositories/RowRequestRepository.java
+++ src/main/java/com/example/tma_warehouse/repositories/RowRequestRepository.java
@@ -1,9 +1,12 @@
 package com.example.tma_warehouse.repositories;
 
 import com.example.tma_warehouse.models.RowRequest.RowRequest;
-import com.example.tma_warehouse.models.request.Request;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface RowRequestRepository extends JpaRepository<RowRequest, Long>, JpaSpecificationExecutor<RowRequest> {
+
+    Page<RowRequest> findByRequestId(Long requestId, Pageable pageable);
 }
