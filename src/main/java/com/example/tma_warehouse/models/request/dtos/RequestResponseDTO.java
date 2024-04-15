928985b417239f059eb30be2d6e299076d546aa3
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-15 13:02:18 +0200

    Builder modified

--- src/main/java/com/example/tma_warehouse/models/request/dtos/RequestResponseDTO.java
+++ src/main/java/com/example/tma_warehouse/models/request/dtos/RequestResponseDTO.java
@@ -3,20 +3,40 @@ package com.example.tma_warehouse.models.request.dtos;
 import lombok.Builder;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record RequestResponseDTO(
         Long requestId,
         String employeeName,
-        Long itemId,
-        String unitOfMeasurement,
-        BigDecimal quantity,
         BigDecimal priceWithoutVat,
         String comment,
         String status,
-        Long requestRowId,
+        //consider later
         String createdAt,
         String modifiedAt
 ) {
     @Builder
-    public RequestResponseDTO {}
+    public static RequestResponseDTO create(Long requestId, String employeeName, BigDecimal priceWithoutVat,
+                                            String comment, String status, LocalDateTime createdAt,
+                                            LocalDateTime modifiedAt) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedCreatedAt = createdAt != null ? createdAt.format(formatter) : null;
+        String formattedModifiedAt = modifiedAt != null ? modifiedAt.format(formatter) : null;
+
+        return new RequestResponseDTO(
+                requestId,
+                employeeName,
+                priceWithoutVat,
+                comment,
+                status,
+                formattedCreatedAt,
+                formattedModifiedAt);
+    }
+
+
+
+
+
+
 }
