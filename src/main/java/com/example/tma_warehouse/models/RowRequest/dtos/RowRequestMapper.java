5aea91aad3d5aa3e7010cd6d1b66a2d15dfbaca6
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-16 00:25:08 +0200

    Mapper and DTO updated

--- src/main/java/com/example/tma_warehouse/models/RowRequest/dtos/RowRequestMapper.java
+++ src/main/java/com/example/tma_warehouse/models/RowRequest/dtos/RowRequestMapper.java
@@ -1,26 +1,22 @@
 package com.example.tma_warehouse.models.RowRequest.dtos;
 
 import com.example.tma_warehouse.models.RowRequest.RowRequest;
-import com.example.tma_warehouse.models.request.Request;
-import com.example.tma_warehouse.models.request.dtos.RequestResponseDTO;
+
+import java.time.LocalDateTime;
 
 public class RowRequestMapper {
 
     public static RowRequestResponseDTO mapToRowRequestResponseDTO(RowRequest rowRequest) {
-        if (rowRequest == null) {
-            return null;
-        }
 
         return RowRequestResponseDTO.builder()
-                .requestRowId(rowRequest.getId())  // The primary key of the RowRequest
                 .requestId(rowRequest.getRequest().getId())  // The foreign key linking to the Request
                 .itemId(rowRequest.getItem().getId())  // The foreign key linking to the Item
                 .unitOfMeasurement(rowRequest.getUnitOfMeasurement().name())  // The unit of measurement as a string
                 .quantity(rowRequest.getQuantity())  // The quantity from RowRequest
-                .priceWithoutVat(rowRequest.getPriceWithoutVAT())  // The price without VAT
+                .priceWithoutVat(rowRequest.getPriceWithoutVat())  // The price without VAT
                 .comment(rowRequest.getComment())  // Any comment associated with the RowRequest
-                .createdAt(rowRequest.getCreatedAt().toString())  // Created at timestamp, converted to string
-                .modifiedAt(rowRequest.getModifiedAt().toString())  // Modified at timestamp, converted to string
+                .createdAt(LocalDateTime.parse(rowRequest.getCreatedAt().toString()))
+                .modifiedAt(LocalDateTime.parse(rowRequest.getModifiedAt().toString())) // Modified at timestamp, converted to string
                 .build();
     }
 }
--- src/main/java/com/example/tma_warehouse/models/RowRequest/dtos/RowRequestResponseDTO.java
+++ src/main/java/com/example/tma_warehouse/models/RowRequest/dtos/RowRequestResponseDTO.java
@@ -1,22 +1,39 @@
 package com.example.tma_warehouse.models.RowRequest.dtos;
 
+import com.example.tma_warehouse.models.request.dtos.RequestResponseDTO;
 import lombok.Builder;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record RowRequestResponseDTO(
         Long requestId,
-        String employeeName,
         Long itemId,
         String unitOfMeasurement,
         BigDecimal quantity,
         BigDecimal priceWithoutVat,
         String comment,
-        String status,
-        Long requestRowId,
         String createdAt,
         String modifiedAt
 ) {
     @Builder
-    public RowRequestResponseDTO {}
+    public static RowRequestResponseDTO create(Long requestId, Long itemId, String unitOfMeasurement, BigDecimal quantity,
+                                               BigDecimal priceWithoutVat,
+                                            String comment, LocalDateTime createdAt,
+                                            LocalDateTime modifiedAt) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedCreatedAt = createdAt != null ? createdAt.format(formatter) : null;
+        String formattedModifiedAt = modifiedAt != null ? modifiedAt.format(formatter) : null;
+
+        return new RowRequestResponseDTO(
+                requestId,
+                itemId,
+                unitOfMeasurement,
+                quantity,
+                priceWithoutVat,
+                comment,
+                formattedCreatedAt,
+                formattedModifiedAt);
+    }
 }
