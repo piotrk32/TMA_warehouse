9886694a6b7aa60e6fa9a2f70be6863ced3d7471
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-15 18:38:18 +0200

    getRequest Controller added

--- src/main/java/com/example/tma_warehouse/controllers/coordinator/CoordinatorRequestController.java
+++ src/main/java/com/example/tma_warehouse/controllers/coordinator/CoordinatorRequestController.java
@@ -2,16 +2,21 @@ package com.example.tma_warehouse.controllers.coordinator;
 
 import com.example.tma_warehouse.exceptions.ErrorMessage;
 import com.example.tma_warehouse.models.item.dtos.ItemResponseDTO;
+import com.example.tma_warehouse.models.request.dtos.RequestRequestDTO;
 import com.example.tma_warehouse.models.request.dtos.RequestResponseDTO;
 import com.example.tma_warehouse.models.request.enums.RequestStatus;
 import com.example.tma_warehouse.services.request.RequestFacade;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -67,5 +72,30 @@ public class CoordinatorRequestController {
     public ResponseEntity<RequestResponseDTO> getRequestById(@PathVariable Long requestId) {
         RequestResponseDTO requestResponseDTO = requestFacade.getRequestById(requestId);
         return ResponseEntity.ok(requestResponseDTO);
+    }
+
+    @GetMapping("/all")
+    @Operation(summary = "Show all requests", description = "Functionality lets user to show all available requests")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successful request acquisition",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = ItemResponseDTO.class)
+                            )
+                    )),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request - temporally returns map of errors or ErrorMessage",
+                    content = @Content(
+                            mediaType = "application/json"
+                    ))
+    })
+    public ResponseEntity<Page<RequestResponseDTO>> getRequests(
+            @ModelAttribute @Valid RequestRequestDTO requestRequestDTO) {
+        Page<RequestResponseDTO> requestResponseDTOPage = requestFacade.getItems(requestRequestDTO);
+        return new ResponseEntity<>(requestResponseDTOPage, HttpStatus.OK);
     }
 }
