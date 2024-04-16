b2ec6f80b16d4d0a1b7677f30fe0e3a8bcb417ad
piotrk32 <piotrkepistyuwm@gmail.com>
2024-04-16 13:56:35 +0200

    unnecessary imports removed

--- src/main/java/com/example/tma_warehouse/repositories/RowRequestRepository.java
+++ src/main/java/com/example/tma_warehouse/repositories/RowRequestRepository.java
@@ -1,12 +1,16 @@
 package com.example.tma_warehouse.repositories;
 
 import com.example.tma_warehouse.models.RowRequest.RowRequest;
+import com.example.tma_warehouse.models.item.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface RowRequestRepository extends JpaRepository<RowRequest, Long>, JpaSpecificationExecutor<RowRequest> {
 
     Page<RowRequest> findByRequestId(Long requestId, Pageable pageable);
+
 }
