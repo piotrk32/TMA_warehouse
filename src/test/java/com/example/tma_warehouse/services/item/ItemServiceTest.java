7288570d48a52e1ae43db95087d5c15141ca0ced
piotrk32 <piotrkepistyuwm@gmail.com>
2024-07-02 17:02:19 +0200

    saveItem_returnsSavedItem created

---
+++ src/test/java/com/example/tma_warehouse/services/item/ItemServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.tma_warehouse.services.item;
+
+import com.example.tma_warehouse.models.item.Item;
+import com.example.tma_warehouse.repositories.ItemRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ItemServiceTest {
+
+    @Mock
+    private ItemRepository itemRepository;
+
+    @InjectMocks
+    private ItemService itemService;
+
+    private Item item;
+
+    @BeforeEach
+    void setUp() {
+        item = new Item();
+        item.setItemName("Test Item");
+    }
+
+    @Test
+    void saveItem_returnsSavedItem() {
+        when(itemRepository.save(any(Item.class))).thenReturn(item);
+
+        Item savedItem = itemService.saveItem(item);
+
+        assertEquals("Test Item", savedItem.getItemName());
+        verify(itemRepository).save(any(Item.class));
+    }
+    @Test
+    void getItemById_returnsItem() {
+        // Arrange
+        when(itemRepository.findById(1L)).thenReturn(Optional.of(item));
+
+        // Act
+        Item foundItem = itemService.getItemById(1L);
+
+        // Assert
+        assertEquals("Test Item", foundItem.getItemName());
+        verify(itemRepository).findById(1L);
+    }
+}
