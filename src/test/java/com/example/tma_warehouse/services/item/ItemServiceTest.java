abf5d3e3f02ea51cc200d9bdb8f0b3095a984514
piotrk32 <piotrkepistyuwm@gmail.com>
2024-07-02 17:59:06 +0200

    getItems_returnsPageOfItems test created

--- src/test/java/com/example/tma_warehouse/services/item/ItemServiceTest.java
+++ src/test/java/com/example/tma_warehouse/services/item/ItemServiceTest.java
@@ -3,6 +3,7 @@ package com.example.tma_warehouse.services.item;
 import com.example.tma_warehouse.exceptions.EntityNotFoundException;
 import com.example.tma_warehouse.models.item.Item;
 import com.example.tma_warehouse.models.item.dtos.ItemInputDTO;
+import com.example.tma_warehouse.models.item.dtos.ItemRequestDTO;
 import com.example.tma_warehouse.models.item.enums.ItemGroup;
 import com.example.tma_warehouse.models.item.enums.UnitOfMeasurement;
 import com.example.tma_warehouse.repositories.ItemRepository;
@@ -12,15 +13,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 public class ItemServiceTest {
 
@@ -124,5 +132,40 @@ public class ItemServiceTest {
         assertEquals(new BigDecimal(15), updatedItem.getQuantity());
         verify(itemRepository).findById(1L);
         verify(itemRepository).saveAndFlush(any(Item.class));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void getItems_returnsPageOfItems() {
+        // Arrange
+        ItemRequestDTO itemRequestDTO = new ItemRequestDTO();
+        itemRequestDTO.setPage("0");
+        itemRequestDTO.setSize("10");
+        itemRequestDTO.setDirection("ASC");
+        itemRequestDTO.setSortParam("itemName");
+
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.Direction.ASC, "itemName");
+        Item item = new Item(
+                "ItemName",
+                ItemGroup.GROUP_A,
+                UnitOfMeasurement.t,
+                new BigDecimal("10"),
+                new BigDecimal("99.99"),
+                "AVAILABLE",
+                "A1",
+                "John Doe",
+                "/images/item.png");
+
+        Page<Item> page = new PageImpl<>(List.of(item), pageRequest, 1);
+
+        when(itemRepository.findAll(any(Specification.class), eq(pageRequest))).thenReturn(page);
+
+        Page<Item> itemsPage = itemService.getItems(itemRequestDTO);
+
+        System.out.println("Total Elements: " + itemsPage.getTotalElements());
+        System.out.println("Items: " + itemsPage.getContent());
+
+        assertEquals(1, itemsPage.getTotalElements());
+        verify(itemRepository).findAll(any(Specification.class), eq(pageRequest));
     }
 }
